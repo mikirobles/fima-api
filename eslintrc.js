5412fa8c483d8166b8626fdc47797e01a3d903c3
Micael Robles <micael.robles@gmail.com>
2018-01-03 12:18:52 -0300

    eslint and git-ignore

---
+++ eslintrc.js
@@ -0,0 +1,36 @@
+const IS_PRODUCTION = process.env.NODE_ENV === "production"
+
+module.exports = {
+    "root": true,
+    "extends": [
+        "standard",
+        "prettier",
+        "eslint:recommended"
+    ],
+    "plugins": [
+        "prettier"
+    ],
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 2016,
+        "sourceType": "module"
+    },
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "node": true,
+        "es6": true,
+        "mocha": true,
+        "jest": true
+    },
+    "rules": {
+        "no-debugger": IS_PRODUCTION ? "error" : "off",
+        "no-console": IS_PRODUCTION ? "error" : "off",
+        "prettier/prettier": [
+            "error",
+            {
+                "semi": false
+            }
+        ]
+    }
+}
